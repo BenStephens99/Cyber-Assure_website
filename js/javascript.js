f0e598a1e6b0648b06d94a21c0f50997d824370b
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-11-21 10:46:57 +0000

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -5,11 +5,12 @@ let dots = document.getElementsByClassName("dot");
 console.log(cards)
 document.getElementById("dots").innerHTML += '<div onClick="goToCard('+0+')" class="dot"></div>';
 cards[0].style.opacity = "1";
-dots[0].style.backgroundColor = "#696969"
-
+cards[0].style.zIndex = "1";
+dots[0].style.backgroundColor = "#696969";
 
 for (let i = 1; i < cards.length; i++) {
     cards[i].style.opacity = "0";
+    cards[i].style.zIndex = "0";
     document.getElementById("dots").innerHTML += '<div onClick="goToCard('+i+')" class="dot"></div>';
 }
 
@@ -17,6 +18,7 @@ dots[0].style.color = "black"
 
 function changeCard(direction) {
     cards[currentCard].style.opacity = "0";
+    cards[currentCard].style.zIndex = "0";
     dots[currentCard].style.backgroundColor = "#bbb"
     currentCard += direction;
     if (currentCard >= cards.length) {
@@ -25,15 +27,18 @@ function changeCard(direction) {
         currentCard = cards.length - 1;
     }
     dots[currentCard].style.backgroundColor = "#696969"
+    cards[currentCard].style.zIndex = "1";
     cards[currentCard].style.opacity = "1";
 
 }
 
 function goToCard(index) {
     cards[currentCard].style.opacity = "0";
+    cards[currentCard].style.zIndex = "0";
     dots[currentCard].style.backgroundColor = "#bbb";
     currentCard = index;
-    dots[index].style.backgroundColor = "#696969"
+    dots[index].style.backgroundColor = "#696969";
+    cards[currentCard].style.zIndex = "1";
     cards[index].style.opacity = "1";
 }
 
